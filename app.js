d0a9bd12c27e21b1738d25528f74da001a59b383
shravanchilka <shravanchilka16@gmail.com>
2024-09-23 00:17:38 +0530

    code changes /shravan endpoint added

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Shravan!');
+    res.send('Ahoy!');
+});
+
+app.get('/shravan', (req, res) => {
+    res.send('Ahoy, This is Shravan!');
 });
 
 const port = 3000;
