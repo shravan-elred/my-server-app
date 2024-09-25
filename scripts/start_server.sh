af1c41aa49d3b0cb09b3842779bd1898e9dae348
shravanchilka <shravanchilka16@gmail.com>
2024-09-25 15:22:58 +0530

    start_server.sh changes to install pm2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user/my-server-app
+# Check if pm2 is installed, if not, install it
+if ! [ -x "$(command -v pm2)" ]; then
+  echo "PM2 not found, installing it..."
+  npm install pm2@latest -g
+fi
 echo "Starting Node.js server..."
 pm2 start app.js
