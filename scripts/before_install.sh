3c67cbb7259627d7d69d745a5d1a31964b07e69f
shravanchilka <shravanchilka16@gmail.com>
2024-09-26 12:13:09 +0530

    script changes - befor_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,3 +4,12 @@ if [ ! -d "/home/ec2-user/my-server-app" ]
 then
   mkdir /home/ec2-user/my-server-app
 fi
+
+cd /home/ec2-user/my-app
+echo "Installing Node.js dependencies..."
+
+# Install dependencies
+npm install
+
+# Install PM2 globally
+npm install -g pm2
