ec3d91e513e883d8db3067a9317e676b01750004
shravanchilka <shravanchilka16@gmail.com>
2024-09-26 09:52:03 +0530

    script changes

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,7 +4,7 @@ export NVM_DIR="$HOME/ .nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-echo "Starting my-ser-app!"
 cd /home/ec2-user/my-server-app
 
-node app.js
+echo "Starting my-server-app!"
+pm2 start app.js --name my-server-app
