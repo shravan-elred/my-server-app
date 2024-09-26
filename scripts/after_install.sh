d2ccb2318260b3b65a9cabbe4ccd93bc64c13c50
shravanchilka <shravanchilka16@gmail.com>
2024-09-26 16:48:39 +0530

    test script changes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-sudo su
 
-# export NVM_DIR="$HOME/ .nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+export NVM_DIR="$HOME/ .nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 cd /home/ec2-user/my-server-app
 
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo su
 
 export NVM_DIR="$HOME/ .nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
